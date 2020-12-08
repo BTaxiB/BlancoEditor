91ae7318a57599485b88c214e707c4b63ea344ec
test <test@test>
2020-12-08 10:00:09 +0100

    included scripts

--- layouts/footer.php
+++ layouts/footer.php
@@ -1,63 +1,12 @@
 <!-- jQuery and JS bundle w/ Popper.js -->
-    <script src="https://code.jquery.com/jquery-3.4.1.min.js"
-        integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous">
-    </script>
-    <script src="assets/editor.js"></script>
-    <script src="assets/tweaks.js"></script>
-    <script type="text/javascript">
-    $(document).ready(() => {
-        loadSelectP('<?php echo $directory; ?>');
-
-        $.ajax({
-            url: 'ajaxP.php',
-            type: 'POST',
-            dataType: 'text',
-            data: {
-                key: 'loadP',
-                dir: "<?php echo $directory; ?>",
-            },
-            success: (response) => {
-                $("#editor").html(response)
-            }
-        });
-
-        $.ajax({
-            url: 'ajaxT.php',
-            type: 'POST',
-            dataType: 'text',
-            data: {
-                key: 'loadT',
-                dir: "<?php echo $directory; ?>",
-            },
-            success: (response) => {
-                $("#tweaks").html(response)
-            }
-        });
-
-        $("#windowT").on('click', () => {
-            $("#path").hide();
-            $("#tweaks").show();
-            $("#path").hide();
-        });
-
-        $("#windowP").on('click', () => {
-            $("#tweaks").hide();
-            $("#path").show();
-            $("#tweaks").hide();
-        });
-
-        // $("input[name='searchFile']").on('change', () => {
-        //     var dir = "<?php echo $directory ?>";
-        //     searchFile(dir);
-        // });
-
-        $(".save").click(() => {
-            saveP();
-        });
-    });
-    </script>
+<script src="https://code.jquery.com/jquery-3.4.1.min.js"
+    integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous">
+</script>
+<script src="assets/js/editor.js"></script>
+<script src="assets/js/tweaks.js"></script>
+<script src="assets/js/targets.js"></script>
 </body>
 
 </html>
