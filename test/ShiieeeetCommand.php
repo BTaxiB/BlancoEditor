692612233fc51ed2e6fa561ef7f7e0f7b7791f37
test <test@test>
2020-12-09 11:53:20 +0100

    reworked test files

---
+++ test/ShiieeeetCommand.php
@@ -0,0 +1,17 @@
+<?php 
+//editable user
+$driver->findElement(WebDriverBy::id('username'));
+
+//editable pass
+$driver->findElement(WebDriverBy::id('password'));
+
+//editable loginBtn
+$driver->findElement(WebDriverBy::id('loginbtn'));
+
+//editable upload page
+$driver->get('urlurlurl.url');
+
+//editable extensions TODO LATER
+$this->setExtensions(['ublock.crx']); //will do laterc
+
+
