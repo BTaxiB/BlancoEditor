59d286a5840d9d21e485156a41a902a1b5139d7e
test <test@test>
2020-12-10 11:57:53 +0100

    editor controller dynamic

--- src/controller/EditorController.php
+++ src/controller/EditorController.php
@@ -4,35 +4,36 @@ namespace JetRefacto\Controller;
 
 use JetRefacto\Boot\Editor;
 
-class EditorController extends Controller {
+class EditorController extends Controller
+{
     // protected static ?string $directory;
     protected static ?string $filename;
     protected static ?object $control;
     protected static ?string $content;
     protected static ?string $message;
 
-    public function __construct() {
+    public function __construct()
+    {
         self::setControl(new Editor);
     }
 
-    public static function toJSON($data) {
+    public static function toJSON($data)
+    {
         echo json_encode($data);
     }
 
-    public static function saveFile($data) {
-        return self::$control::save(self::getFile(), $data);
+    public static function saveFile()
+    {
+        return self::$control::save(self::getFile(), self::getData());
     }
 
-    public function edit() {
-        $data = $this->control::format($_POST['data'], $_POST['file']);
-        $message = $this->control::save($_POST['file'], $data);
-        $file = $this->control::getData($_POST['file']);
-
-        self::getFile();
+    public function edit()
+    {
+        self::setData($this->control::format($_POST['data'], self::getFile()));
 
         self::toJSON([
-            'html' => $file,
-            'message' => $message,
+            'html' => $this->control::getData(self::getFile()),
+            'message' => $this->control::save(self::getFile(), self::getData()),
         ]);
     }
 }
